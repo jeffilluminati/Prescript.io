2b3ddef85a74c7f41077f605d468c057bbd9b748
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-08-21 22:08:22 +0800

    Made some gooey edits

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,29 @@
 package application;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class Main extends Application {
     public static Stage stage;
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         stage = primaryStage;
+        stage.setScene(loadDelivery());
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+    }
+
+    public Scene loadDelivery() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/delivery/delivery.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Main.class.getResource("view/delivery/deliveryStyle.css").toExternalForm());
+        return scene;
     }
 }
