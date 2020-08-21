8b086379cc60ea6882feaa7edcdb6c5e3aeb5138
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-08-21 20:00:21 +0800

    Added files.

--- hackathon/src/application/model/delivery/Deliverer.java
+++ hackathon/src/application/model/delivery/Deliverer.java
@@ -2,10 +2,14 @@ package application.model.delivery;
 
 import application.model.base.*;
 
+import java.util.ArrayList;
+
 public class Deliverer extends Stakeholder {
     /*
     Prannaya, please do this file.
      */
+
+    private ArrayList<String> tasks;
 
     public Deliverer(String name) {
         super(name, "Deliverer");
---
+++ hackathon/src/application/model/delivery/Prescription.java
@@ -0,0 +1,38 @@
+package application.model.delivery;
+
+import application.model.client.Client;
+import application.model.doctor.Doctor;
+
+public class Prescription {
+    Doctor doctor; Client client; String prescription;
+
+    public Prescription(Doctor doctor, Client client, String prescription) {
+        this.doctor = doctor;
+        this.client = client;
+        this.prescription = prescription;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(String prescription) {
+        this.prescription = prescription;
+    }
+}
