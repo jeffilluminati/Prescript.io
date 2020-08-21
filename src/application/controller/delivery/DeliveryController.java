59be1e20fc630434be9c9fd65adbf520c3ffa324
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-08-22 02:23:57 +0800

    made some gooey edits

--- src/application/controller/delivery/DeliveryController.java
+++ src/application/controller/delivery/DeliveryController.java
@@ -8,48 +8,42 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class DeliveryController implements Initializable {
     private double xOffset = 0;
     private double yOffset = 0;
-    @FXML
-    private BorderPane border_pane;
+    @FXML private BorderPane border_pane;
+    @FXML private VBox content_area;
+    @FXML private HBox menubar;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         makeStageDrageable();
-        try {
-            Parent contentarea = FXMLLoader.load(getClass().getResource("/application/view/delivery/content.fxml"));
-            border_pane.setCenter(contentarea);
-        } catch (IOException | NullPointerException ex) {
-            Logger.getLogger(DeliveryController.class.getName()).log(Level.SEVERE, null, ex);
-        }
 
     }
 
     private void makeStageDrageable() {
-        border_pane.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = event.getSceneX();
-                yOffset = event.getSceneY();
-            }
+        border_pane.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
         });
-        border_pane.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                Main.stage.setX(event.getScreenX() - xOffset);
-                Main.stage.setY(event.getScreenY() - yOffset);
-                Main.stage.setOpacity(0.7f);
-            }
+        border_pane.setOnMouseDragged(event -> {
+            Main.stage.setX(event.getScreenX() - xOffset);
+            Main.stage.setY(event.getScreenY() - yOffset);
+            Main.stage.setOpacity(0.7f);
         });
         border_pane.setOnDragDone((e) -> {
             Main.stage.setOpacity(1.0f);
