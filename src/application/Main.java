e6e2f952cbf09a8347301c433b58c136fbb6fb5e
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-08-22 02:43:27 +0800

    made some gooey edits

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,8 +5,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
     public static Stage stage;
@@ -15,13 +17,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         stage = primaryStage;
         stage.setScene(loadDelivery());
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
     public Scene loadDelivery() throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("application/view/delivery/delivery.fxml"));
+        URL url = getClass().getResource("view/delivery/delivery.fxml");
+        Parent root = FXMLLoader.load(url);
         Scene scene = new Scene(root);
-        // scene.getStylesheets().add(Main.class.getResource("view/delivery/style.css").toExternalForm());
+        scene.getStylesheets().add(Main.class.getResource("view/delivery/style.css").toExternalForm());
         return scene;
     }
 }
