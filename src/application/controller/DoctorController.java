3a3462ad40a14801ce2019f5b09e67763dead6a2
jeffilluminati <mayukh4321@gmail.com>
2020-08-22 04:28:31 +0800

    updated doctor

--- src/application/controller/DoctorController.java
+++ src/application/controller/DoctorController.java
@@ -1,15 +1,81 @@
 package application.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.*;
-import javafx.scene.layout.VBox;
 
 public class DoctorController implements Initializable {
-    @FXML VBox win;
+    @FXML
+    public Accordion leftAccordion;
+    @FXML
+    public Label patientNameLabel;
+    @FXML
+    public TextField patientNameTF;
+    @FXML
+    public Label ICLabel;
+    @FXML
+    public TextField ICTF;
+    @FXML
+    public Label addressLabel;
+    @FXML
+    public TextField addressTF;
+    @FXML
+    public Label prescriptionNameLabel;
+    @FXML
+    public TextField prescriptionNameTF;
+    @FXML
+    public Label prescriptionDescriptionLabel;
+    @FXML
+    public TextField prescriptionDescriptionTF;
+    @FXML
+    public Label errorLabel;
+    @FXML
+    public Button addPatientBtn;
+    @FXML
+    public Button editPatientBtn;
+    @FXML
+    public Button deletePatientBtn;
+    @FXML
+    public Button editPrescriptionButton;
+    @FXML
+    public Button saveBtn;
+
+
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    public void onAddPatientBtn(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void onEditPatientBtn(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void onDeletePatientBtn(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void onEditPrescriptionBtn(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onSaveBtn(ActionEvent actionEvent) {
+
+    }
+
 }
