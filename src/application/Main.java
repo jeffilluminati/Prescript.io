dfda3483b4728aef298f7d504efa863bfa911a35
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-08-21 21:52:20 +0800

    Made some edits

---
+++ src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public class Main extends Application {
+    public static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) {
+        stage = primaryStage;
+    }
+}
