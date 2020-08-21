bad1b5f577f3488e8bd35b5c963378d5f04edb1b
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-08-21 20:30:24 +0800

    Added files.

--- hackathon/src/application/model/delivery/Deliverer.java
+++ hackathon/src/application/model/delivery/Deliverer.java
@@ -12,13 +12,23 @@ public class Deliverer extends Stakeholder {
 
     private ArrayList<Prescription> deliveries;
 
-    public Deliverer(String name) {
-        super(name, "Deliverer");
+    public Deliverer(String name, String address) {
+        super(name, "Deliverer", address);
         deliveries = new ArrayList<>();
     }
 
     public Deliverer() {
-        this("");
+        this("", "");
+    }
+
+    /*
+    format of each line in delivers.csv: <name>,<address>
+     */
+
+    public static Deliverer parse(String str) {
+        String[] arr = str.split(",");
+        String name = arr[0], address = String.join(",", Arrays.copyOfRange(arr, 1, arr.length));
+        return new Deliverer(name, address);
     }
 
     public void addDeliveries(Prescription ...prescriptions) {
