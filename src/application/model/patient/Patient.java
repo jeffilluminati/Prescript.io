15489247d0701c3476b5d49d6ef719a4ada26f6c
ovenprint <pinventor7@gmail.com>
2020-08-21 21:25:08 +0800

    rename client to patient

--- src/application/model/patient/Patient.java
+++ src/application/model/patient/Patient.java
@@ -1,9 +1,26 @@
 package application.model.patient;
 
 import application.model.base.*;
+import application.model.doctor.Doctor;
+
+import java.util.ArrayList;
 
 public class Patient extends Stakeholder {
-    /*
-    Joshua, please do this file.
-     */
+    private ArrayList<Prescription> prescriptions;
+    private ArrayList<Doctor> doctors;
+
+    private void addPrescription(Prescription p) { prescriptions.add(p); }
+    private void removePrescription(Prescription p) { prescriptions.remove(p); }
+
+    private void addDoctor(Doctor d) { doctors.add(d); }
+    private void removeDoctor(Doctor d) { doctors.remove(d); }
+
+
+    public Patient(String name, String address) {
+        super(name,address);
+        prescriptions = new ArrayList<>();
+        doctors = new ArrayList<>();
+    }
+
+
 }
