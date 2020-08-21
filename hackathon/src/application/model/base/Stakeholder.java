10e39a78bf558a639cc34d18844e860b103f99d1
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-08-21 19:53:21 +0800

    Added files.

--- hackathon/src/application/model/base/Stakeholder.java
+++ hackathon/src/application/model/base/Stakeholder.java
@@ -4,4 +4,24 @@ public class Stakeholder {
     /*
     Base class that all others extend from
      */
+
+    private String name, role;
+
+    public Stakeholder(String name, String role) {
+        this.name = name; this.role = role;
+    }
+
+    public Stakeholder(String role) {
+        this("", role);
+    }
+
+    public Stakeholder() { this(""); }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
