2d44acdd19c8202d550773338af0c1688d9a80b2
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-08-21 20:10:27 +0800

    Added files.

--- hackathon/src/application/controller/ClientController.java
+++ hackathon/src/application/controller/ClientController.java
@@ -3,9 +3,10 @@ package application.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.*;
+import javafx.scene.layout.VBox;
 
 public class ClientController implements Initializable {
-
+    @FXML VBox win;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
--- hackathon/src/application/controller/DeliveryController.java
+++ hackathon/src/application/controller/DeliveryController.java
@@ -1,10 +1,13 @@
 package application.controller;
 
 import javafx.fxml.*;
+import javafx.scene.layout.VBox;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DeliveryController implements Initializable {
+    @FXML VBox win;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
