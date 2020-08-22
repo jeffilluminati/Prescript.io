387077d9aa4c913747c4e15d636b78e78ddb8196
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-08-22 11:42:10 +0800

    made some gooey edits

--- src/application/controller/login/LoginController.java
+++ src/application/controller/login/LoginController.java
@@ -1,4 +1,11 @@
 package application.controller.login;
 
+import application.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoginController {
+    @FXML public void patient(ActionEvent event) { PatientMain.launch(); }
+    @FXML public void doctor(ActionEvent event) { DoctorMain.launch(); }
+    @FXML public void deliverer(ActionEvent event) { DeliveryMain.launch(); }
 }
