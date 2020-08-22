e685d007388f26f6cfe022b831a0a1c290515a6d
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-08-22 10:14:14 +0800

    made some gooey edits

--- src/application/controller/delivery/AdminController.java
+++ src/application/controller/delivery/AdminController.java
@@ -1,13 +1,22 @@
 package application.controller.delivery;
 
-import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AdminController implements Initializable {
+    @FXML private Label name, address;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        name.setText(DeliveryController.getSelf().getName());
+        address.setText(address.getText()+DeliveryController.getSelf().getAddress());
+    }
 
+    @FXML
+    public void cancel(ActionEvent event) {
+        DeliveryController.currentOccurence.setMain(event);
     }
 }
